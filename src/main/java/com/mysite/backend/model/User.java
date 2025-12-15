6d07ff4100e91c0ab95487489f9bfddcb00d4d86
SeongJaeBak <uuuu8882979@gmail.com>
2025-12-15 09:36:35 +0900

    main

---
+++ src/main/java/com/mysite/backend/model/User.java
@@ -0,0 +1,20 @@
+package com.mysite.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String name;
+    private String email;
+}
